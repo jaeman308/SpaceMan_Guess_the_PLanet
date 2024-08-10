713b9a67697a3e0a7b39133231369cdadb7db972
jborruel308@me.com <jborruel308@me.com>
2024-08-09 23:05:09 -0700

    update play again btn and spelled check hints

--- js/app.js
+++ js/app.js
@@ -7,10 +7,10 @@ const wordBank = [
          hint: "Known as Earth's sister planet due to its similar size, it has a thick, toxic atmosphere and is the hottest planet."
     },
     {   word: "EARTH",
-        hint:"Our home planet, it has a unique atmosphere that supports life and is the onlyknown planet with liquid water on its surface."
+        hint:"Our home planet, it has a unique atmosphere that supports life and is the only known planet with liquid water on its surface."
     },
     {   word: "MARS",
-        hint: "Often call the Red Planet because of its reddish appearance, it has the tallest volcano and the deepest canyon in the solar system."
+        hint: "Often called the Red Planet because of its reddish appearance, it has the tallest volcano and the deepest canyon in the solar system."
     },
     {   word: "JUPITER",
         hint: "The largest planet in the solar system,  it has a Great Red Spot that is a massive storm  and is known for its many moons."
@@ -22,7 +22,7 @@ const wordBank = [
         hint: "This planet is unique for its extreme tilt, which causes it to roll on its side as it orbits the Sun."
     },
     {   word: "NEPTUNE",
-        hint: "Known for its deep blue color due to its methane atomsphere, it has strong winds and is the farthest planet from the Sun."
+        hint: "Known for its deep blue color due to its methane atmosphere, it has strong winds and is the farthest planet from the Sun."
     },
     {   word: "PLUTO",
         hint: "Once considered the ninth planet, it is now classified as a dwarf planet. It has a highly elliptical orbit and a surface with icy plains and mountains."
@@ -132,7 +132,7 @@ function updateMessage() {
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-playAgainBtn.addEventListener('click', init);
+playAgainBtn.addEventListener('click', startGame);
 startBtn.addEventListener('click', startGame)
 document.querySelectorAll('.alphabetBtn').forEach(button => {
     button.addEventListener('click', handleButtonClick);
